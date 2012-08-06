2e5355f95c6155dc711a67a2d973384113b8bba6
Lucas Lima de Souza <lucaslima4p@gmail.com>
2012-08-06 12:12:34 -0300

    tests of profile model

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Profile do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "CRUD" do
+
+    before(:each) do
+       @profile = Profile.new(name: "Lucas", nickname: "Lucas Pintor", cell_phone: "(35) 000-000", phone: "(35)000-000", experience: "Ha 20 anos trabalhando com pintura")
+
+    end
+    context "create" do
+
+      it "should be saved" do
+        @profile.save.should be_true
+      end
+
+    end
+
+    context "validations" do
+      it "should validate presence of" do
+          @profile.valid?.should be_true
+      end
+    end
+  end
 end
