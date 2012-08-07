afe9b388f881efc2ca327f1545faf491a55d6e18
Lucas Lima de Souza <lucaslima4p@gmail.com>
2012-08-07 10:30:53 -0300

    Validates presence of attibutes profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,4 @@
 class Profile < ActiveRecord::Base
   attr_accessible :cell_phone, :experience, :name, :nickname, :phone
+  validates_presence_of :name, :cell_phone, :phone, :experience
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -5,20 +5,47 @@ describe Profile do
   context "CRUD" do
 
     before(:each) do
-       @profile = Profile.new(name: "Lucas", nickname: "Lucas Pintor", cell_phone: "(35) 000-000", phone: "(35)000-000", experience: "Ha 20 anos trabalhando com pintura")
+       @profile = Profile.new(valid_profile_hash)
 
     end
 
-    context "create" do
-      it "should be saved" do
-        @profile.save.should be_true
-      end
+    it "should be saved" do
+      @profile.save.should be_true
     end
+
 
     context "validations" do
-      it "should validates presence of" do
-          @profile.valid?.should be_true
+      it "should validates presence of name" do
+        @profile.name = ""
+        @profile.invalid?(:name).should be_true
+        @profile.errors[:name].should eql(["can't be blank"])
+        @profile.should_not be_valid
+      end
+
+      it "should validates presence of cell phone" do
+        @profile.cell_phone = ""
+        @profile.invalid?(:cell_phone).should be_true
+        @profile.errors[:cell_phone].should eql(["can't be blank"])
+        @profile.should_not be_valid
+      end
+
+      it "should validates presence of phone" do
+        @profile.phone =""
+        @profile.invalid?(:phone).should be_true
+        @profile.errors[:phone].should eql(["can't be blank"])
+        @profile.should_not be_valid
+      end
+
+      it "should validates presence of experience" do
+        @profile.experience =""
+        @profile.invalid?(:experiece).should be_true
+        @profile.errors[:experience].should eql(["can't be blank"])
+        @profile.should_not be_valid
       end
     end
+  end
+
+  def valid_profile_hash
+    {name: "Lucas", nickname: "Lucas Pintor", cell_phone: "(35) 000-000", phone: "(35)000-000", experience: "Ha 20 anos trabalhando com pintura"}
   end
 end
